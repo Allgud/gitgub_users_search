47c1718310b8f41c5497fa9d95ad308e578d4782
Allgud <allgud83@gmail.com>
2023-07-29 13:18:29 +0300

    add search by login

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,6 @@
 import SearchInput from '../SearchInput';
 import styles from './app.module.css'
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import UserCard from '../UserCard';
 import Pagination from '../Pagination'
 import { PUBLIC_API, PAGE_CARDS_COUNT, DEFAULT_PAGE_NUMBER } from "../../constants";
@@ -11,14 +11,21 @@ const App = () => {
   const [users, setUsers] = useState<User[] | []>([])
   const [totalPages, setTotalPages] = useState<number>(DEFAULT_PAGE_NUMBER)
 
-  const togglePage = useCallback(async (page: number) => {
+  const togglePage = async (page: number) => {
     const response = await fetch(`${PUBLIC_API}?q=Q&per_page=${PAGE_CARDS_COUNT}&page=${page}`)
     const result = await response.json()
     setUsers(result.items)
-  }, [])
+  }
 
   const getData = async () => {
     const response = await fetch(`${PUBLIC_API}?q=Q&per_page=${PAGE_CARDS_COUNT}`)
+    const result = await response.json()
+    setTotalPages(getTotalPageNumber(result.total_count))
+    setUsers(result.items)
+  }
+
+  const searchUser = async (text: string) => {
+    const response = await fetch(`${PUBLIC_API}?q=${text}&per_page=${PAGE_CARDS_COUNT}`)
     const result = await response.json()
     setTotalPages(getTotalPageNumber(result.total_count))
     setUsers(result.items)
@@ -31,7 +38,7 @@ const App = () => {
   return (
     <div className={styles.app__wrapper}>
       <div className={styles.app__container}>
-        <SearchInput />
+        <SearchInput onSearchClick={searchUser}/>
         <ul className={styles.results__list}>
           {users?.map(user => (
             <li key={user.id} className={styles.list__item} onClick={() => togglePage(3)}>
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,12 +1,28 @@
 import Button from '../Button'
-
+import { useState } from 'react'
 import styles from './search-input.module.css'
 
-const SearchInput = () => {
+type SearchInputProps = {
+  onSearchClick: (arg1: string) => {};
+}
+
+const SearchInput = ({ onSearchClick }: SearchInputProps) => {
+  const [text, setText] = useState<string>('')
+
+  const onClickButton = () => {
+    onSearchClick(text)
+    setText('')
+  }
+
   return (
     <div className={styles.search__group}>
-      <input type="text" className={styles.search__input} placeholder='Поиск...'/>
-      <Button text='Найти' onClick={() => {}}/>
+      <input
+        type="text"
+        className={styles.search__input}
+        placeholder='Поиск...'
+        value={text}
+        onChange={(evt) => setText(evt.target.value)} />
+      <Button text='Найти' onClick={() => onClickButton()}/>
     </div>
   )
 }
